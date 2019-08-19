0d9e65f6a3108d9c7819d609ceb5fda781310ee0
DUFAY Cyril <>
2019-08-19 17:09:23 +0200

    fix typo

--- lib/src/admob.dart
+++ lib/src/admob.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 class Admob {
   static String testDeviceId;
 
-  Admob.initialize({String appId}) {
+  Admob.initialize(String appId) {
     MethodChannel _channel = const MethodChannel('admob_flutter');
     _channel.invokeMethod('initialize', appId);
   }
